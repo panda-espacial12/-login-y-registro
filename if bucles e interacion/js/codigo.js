90223aa2c1fb458f0ba493c90183f658fab2c2f2
Alexander <alexsanderseisdedos@gmail.com>
2024-02-28 14:15:31 -0300

    login-registro

---
+++ if bucles e interacion/js/codigo.js
@@ -0,0 +1,21 @@
+// if pregunta una sola vez y ejecuta 
+//let numero = 0;
+
+//if (numero < 10){  
+   // numero++; //el ++ aumenta 1
+ //   document.write(numero);  //documen.write()muestra en pantalla lo de parentecis
+//}
+
+// if pregunta una sola vez y ejecuta 
+//while pregunta siempre
+// do while se hace la pregunta y despues se ejecuta
+
+//while pregunta siempre
+
+let numero = 0;
+
+while (numero < 10) {
+    numero++;
+    document.write(numero)
+}
+
